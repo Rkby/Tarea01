a5a2653d62513e082256c7adda99ea7cde7972c3
rckby <va.quintero23@info.uas.edu.mx>
2024-03-01 16:55:49 -0700

    commit español

---
+++ README-es.md
@@ -0,0 +1,1 @@
+para compilar el programa gcc prog-01.c y para ejecutar ./ el archivo de salida
---
+++ prog-01.c
@@ -0,0 +1,13 @@
+#include  <stdio.h>
+int main(){
+printf("Hola mundo");
+
+
+return 0;
+
+
+
+
+
+
+}
